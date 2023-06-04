84af5594b49c486abe7ae12e6ce546469bb2e526
jheiser81 <jheiser81@gmail.com>
2023-06-03 17:31:46 -0700

    First python code

---
+++ Python/Rock, Paper, Scissors.py
@@ -0,0 +1,11 @@
+
+def get_choices (): #def is how you define a function in python
+    player_choice = "rock"
+    computer_choice = "paper"
+
+    return computer_choice
+
+    #call the get_choices function, and store the response in a variable called choices
+
+    choices = get_choices
+    print (choices)
