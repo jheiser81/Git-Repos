7d3a89e2c9a0aad3cdaabeebee10f8fb455daae9
jheiser81 <jheiser81@gmail.com>
2023-10-31 00:24:06 -0700

    init

--- 100 Days of Code/Band_Name_Gen.py
+++ 100 Days of Code/Band_Name_Gen.py
@@ -1,4 +1,11 @@
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city or town you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be " + city + " " + pet)
+# print("Welcome to the Band Name Generator.")
+# city = input("What's the name of the city or town you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + city + " " + pet)
+
+# print("Welcome to the Band Name Generator.")
+# city = input("What's the name of the city or town you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print(f"Your band name could be {city} {pet}")
+
+
---
+++ 100 Days of Code/Day 1 - Variables and Strings.py
@@ -0,0 +1,18 @@
+print("Hello, World!\nHello, World!\nHello, World!")
+print("Hello" + " " + "Joel")
+
+# Fix the code below 👇
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+# Missing double quotes
+print('String concatenation is done with "+" sign')
+print("String Concatenation is done with the \"+\" sign.")
+# + sign read as string, need to differentiate quotes, or escape string with \
+print('e.g. print("Hello " + "world")')
+# Unnecessary indent, in python this will cause an error
+print("New lines can be created with a backslash and n.")
+# Extra bracket
