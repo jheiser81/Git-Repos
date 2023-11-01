1876c33331ef072df2db621b2fe9c6f7ce1ac174
jheiser81 <jheiser81@gmail.com>
2023-10-31 23:19:33 -0700

    covered input and len functions

--- 100 Days of Code/Day 1 - Variables and Strings.py
+++ 100 Days of Code/Day 1 - Variables and Strings.py
@@ -16,3 +16,27 @@ print('e.g. print("Hello " + "world")')
 # Unnecessary indent, in python this will cause an error
 print("New lines can be created with a backslash and n.")
 # Extra bracket causing error
+
+input("What is your name?")
+# the input() function will prompt the user for input and return the value as a string.
+
+print("Hello " + input("What is your name?"))
+# In this example, the input is passed as an argument to the print function which is then concatenated with the string
+# "Hello " and printed to the console as a single string value "Hello <name>".
+# This is an example of nested function calls, since we are calling the print function with the input function inside
+
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+# In this example, the input is passed to the int() function, which converts the string value to an integer.
+# The resulting integer values are then multiplied together and printed to the console.
+
+# Write a program that prints the number of characters in a user's name
+print(len(input("What is your name?")))
+# The len() function returns the number of characters in a string. In this example, the len function is called with the
+# input function as an argument. The input function prompts the user for input and returns the value as a string.
+# The len function then returns the number of characters in the string and prints it to the console.
+
+name = input("What is your name? ")
+print(len(name))
+# In this example, I have assigned the input value to a variable called 'name'. Everything else is the same as above.
