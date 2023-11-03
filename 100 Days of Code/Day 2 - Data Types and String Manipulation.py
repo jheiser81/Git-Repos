b5c5eaefd1eaa9ccd49ab9a1baecb7d79480c325
jheiser81 <jheiser81@gmail.com>
2023-11-02 20:08:59 -0700

    Added string data type

---
+++ 100 Days of Code/Day 2 - Data Types and String Manipulation.py
@@ -0,0 +1,9 @@
+# **Primitive Data Types**
+
+# **String**
+
+# Strings are just a sequence of characters. In python, they can be enclosed in single or double quotes. 
+
+print("Hello"[0])
+# This will print the first character in the string "Hello", which is "H". This uses the subscript operator to access the character at the specified index. 
+# In python, the first character in a string always starts at index 0.
