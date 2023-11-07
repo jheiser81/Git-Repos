e8df2fadd41fb09af429d82674b9629435d64b6d
jheiser81 <jheiser81@gmail.com>
2023-11-06 20:54:52 -0800

    Project complete

--- 100 Days of Code/Project - BMI_Calculator.py
+++ 100 Days of Code/Project - BMI_Calculator.py
@@ -8,3 +8,10 @@ print(int(bmi))
 # Both the height input and weight input need to be converted to floats, because the input() function returns a string.
 # The height input is also raised to the power of 2, using the exponent operator "**".
 # The result is then converted to an integer, using the int() function.
+
+# An alternative way to solve this is:
+
+bmi = weight / (height * height)
+print(int(bmi))
+
+# This works because the calculation inside the brackets is evaluated first, and then the rest of the calculation is performed.
