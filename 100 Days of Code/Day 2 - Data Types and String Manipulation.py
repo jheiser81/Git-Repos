54058a712fc917a1faebe2d1ea697124af309e7d
jheiser81 <jheiser81@gmail.com>
2023-11-06 20:07:07 -0800

    ...

--- 100 Days of Code/Day 2 - Data Types and String Manipulation.py
+++ 100 Days of Code/Day 2 - Data Types and String Manipulation.py
@@ -1,92 +1,93 @@
-# # **Primitive Data Types**
+# **Primitive Data Types**
 
-# # **Strings**
+# **Strings**
 
-# # Strings are just a sequence of characters. In python, they can be enclosed in single or double quotes. 
+# Strings are just a sequence of characters. In python, they can be enclosed in single or double quotes. 
 
-# print("Hello"[0])
-# # This will print the first character in the string "Hello", which is "H". This uses the subscript operator to access the character at the specified index. 
-# # In python, the first character in a string always starts at index 0.
+print("Hello"[0])
+# This will print the first character in the string "Hello", which is "H". This uses the subscript operator to access the character at the specified index. 
+# In python, the first character in a string always starts at index 0.
 
-# street_name = "Abbey Road"
-# print(street_name[4] + street_name[7])
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
 
-# print("123" + "456")
-# # Note that the "numbers" inside the quotes are not treated as actual numbers, but as characters in a string. This is because the computer reads anything inside quotes as a string, and so the result will simply concatenate the two strings together.
+print("123" + "456")
+# Note that the "numbers" inside the quotes are not treated as actual numbers, but as characters in a string. This is because the computer reads anything inside quotes as a string, and so the result will simply concatenate the two strings together.
 
-# # **Integers**
+# **Integers**
 
-# # Integers are just whole numbers. They can be positive or negative. 
-# # To get the computer to read the above example as numbers, you can simply remove the quotes. In python, numbers not enclosed in quotes are automatically treated as integers. In other languages, this may not be the case, and you may have to specify that the number is an integer first.
+# Integers are just whole numbers. They can be positive or negative. 
+# To get the computer to read the above example as numbers, you can simply remove the quotes. In python, numbers not enclosed in quotes are automatically treated as integers. In other languages, this may not be the case, and you may have to specify that the number is an integer first.
 
-# print(123 + 456)
+print(123 + 456)
 
-# # In normal usage, large integers are written with commas to separate them. In python, you can use underscores to accomplish the same separation. When the computer reads the number, it will ignore the underscores and treat the number as a single integer. The underscores are simply there to make the number easier to read for humans.
+# In normal usage, large integers are written with commas to separate them. In python, you can use underscores to accomplish the same separation. When the computer reads the number, it will ignore the underscores and treat the number as a single integer. The underscores are simply there to make the number easier to read for humans.
 
-# print(123_456_789)
+print(123_456_789)
 
-# # **Floats(Floating Point Numbers)**
+# **Floats(Floating Point Numbers)**
 
-# # Floating point numbers are simply numbers with a decimal point. They can be positive or negative.
+# Floating point numbers are simply numbers with a decimal point. They can be positive or negative.
 
-# print(3.14159)
+print(3.14159)
 
-# # **Boolean**
+# **Boolean**
 
-# # Booleans are simply True or False values. They are used for checking conditions in code, and activating or ignoring certain parts of the code depending on the result of the check.
+# Booleans are simply True or False values. They are used for checking conditions in code, and activating or ignoring certain parts of the code depending on the result of the check.
 
-# bool = True
-# bool = False
+bool = True
+bool = False
 
-# # **Type Error, Type Checking and Type Conversion**
+# **Type Error, Type Checking and Type Conversion**
 
-# # **Type Error**
+# **Type Error**
 
-# # A type error occurs when you try to combine two different data types in a way that is not supported. For example, you cannot add a string and an integer together.
+# A type error occurs when you try to combine two different data types in a way that is not supported. For example, you cannot add a string and an integer together.
 
-# num_char = len(input("What is your name? "))
-# # print("Your name has " + num_char + " characters.")
+num_char = len(input("What is your name? "))
+# print("Your name has " + num_char + " characters.")
 
-# # This will result in a type error, because you are trying to combine a string and an integer together. The len() function returns an integer, and the "+" operator is used to concatenate strings together. 
+# This will result in a type error, because you are trying to combine a string and an integer together. The len() function returns an integer, and the "+" operator is used to concatenate strings together. 
 
-# # **Type Checking**
+# **Type Checking**
 
-# # To check the data type of a variable, you can use the type() function.
-# print(type(num_char))
+# To check the data type of a variable, you can use the type() function.
+print(type(num_char))
 
-# # **Type Conversion**
+# **Type Conversion**
 
-# # In addition to type checking, you can also perform type conversion or "type casting". This is when you convert one data type to another. For example, you can convert an integer to a string, or a string to an integer.
+# In addition to type checking, you can also perform type conversion or "type casting". This is when you convert one data type to another. For example, you can convert an integer to a string, or a string to an integer.
 
-# num_char = len(input("What is your name? "))
-# new_num_char = str(num_char)
-# print("Your name has " + new_num_char + " characters.")
+num_char = len(input("What is your name? "))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
 
-# a = 123
-# print(type(a))
-# # This will print the data type of the variable a, which is an integer.
+a = 123
+print(type(a))
+# This will print the data type of the variable a, which is an integer.
 
-# a = str(123)
-# print(type(a))
+a = str(123)
+print(type(a))
 # This will print the data type of the variable a, which has been converted to a string.
 
-# a = float(123)
-# print(type(a))
+a = float(123)
+print(type(a))
 
-# print(70 + float("100.5")) This will print the sum of 70 and 100.5, which is 170.5. The string "100.5" has been converted to a float, and the two numbers have been added together.
+print(70 + float("100.5")) 
+# This will print the sum of 70 and 100.5, which is 170.5. The string "100.5" has been converted to a float, and the two numbers have been added together.
 
-# print(str(70) + str(100))
+print(str(70) + str(100))
 # This will simply concatenate the two strings together, resulting in "70100".
 
-# **Coding Exercise**
-# In this exercise, the goal is to write a program that takes a two-digit number input, and then adds the two digits together.
+#**Coding Exercise**
+#In this exercise, the goal is to write a program that takes a two-digit number input, and then adds the two digits together.
 
-# two_digit_number = input("Type a two digit number: ")
-# # print(type(two_digit_number))
-# first_digit = int(two_digit_number[0])
-# second_digit = int(two_digit_number[1])
-# two_digit_number = first_digit + second_digit
-# print(two_digit_number)
+two_digit_number = input("Type a two digit number: ")
+# print(type(two_digit_number))
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
 
 # **Mathematical Operations in Python**
 print(3 + 5)  # Addition
