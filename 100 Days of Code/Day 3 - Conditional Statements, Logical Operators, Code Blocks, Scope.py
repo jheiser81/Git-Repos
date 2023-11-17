e4705b9d1e0a490db171626d9cba526921f403a7
jheiser81 <jheiser81@gmail.com>
2023-11-17 08:59:40 -0800

    Add conditional statements and comparison operators

---
+++ 100 Days of Code/Day 3 - Conditional Statements, Logical Operators, Code Blocks, Scope.py
@@ -0,0 +1,34 @@
+# ======================================================== #
+# ======== Conditional Statements (if, else, elif) ======= #
+# ======================================================== #
+
+# A basic if/else statement in python looks like this:
+water_level = 50
+if water_level > 80:
+    print("Drain water")
+else:
+    print("Continue filling")
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You are allowed to ride the rollercoaster.") 
+else:
+    print("Sorry, you are too short to ride.")
+# Essentially, an if/else statement is a boolean true/false statement with certain conditions. If the condition is true, a particular thing will happen. If the condition is not true (false) then something else will happen
+# In Python, if/else statements end with a colon to denote the end of the statement. The next line is indented, to show that it is part of the previous code block. This is different from other languages, which use curly braces to denote code blocks. Python instead uses indentation to denote code blocks, so it is important to use indentation properly, or the code will throw and error.
+
+# ======================================================== #
+# ================= Comparison Operators ================= #
+# ======================================================== #
+
+# The basic comparison operators in Python are the same as most other programming languages:
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+# It is important to remember than a single = is the assignment operator. It is used to "asign" a value to a variable, whereas two == is the equality operator, and is used to compare two values against each other.
+
