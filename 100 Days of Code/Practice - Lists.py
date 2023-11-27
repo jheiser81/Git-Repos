934caad54e4cbf66c17053ead16de741b1d681fd
jheiser81 <jheiser81@gmail.com>
2023-11-26 19:48:02 -0800

    Refactor code: Remove unnecessary code and create nested list for fruits and vegetables

--- 100 Days of Code/Practice - Lists.py
+++ 100 Days of Code/Practice - Lists.py
@@ -31,15 +31,29 @@
 
 # Simpler solution:
 
-import random
+# import random
 
-names_string = input("Enter a list of names separated by commas and a space: ")
-names = names_string.split(", ")
-name_index = random.randrange(len(names))
-random_name = names[name_index]
+# names_string = input("Enter a list of names separated by commas and a space: ")
+# names = names_string.split(", ")
+# name_index = random.randrange(len(names))
+# random_name = names[name_index]
 
-print(f"{random_name} is going to buy the meal today!")
+# print(f"{random_name} is going to buy the meal today!")
 
 # This is a simpler and more efficient solution, because it uses the randrange() function, which returns a random integer between 0 and the number of items in the list, and then uses that random integer as the index position to return the name at that index position. In this way, it avoids the need to use the randint() function and having to subtract 1 from the number of items in the list, since this is done automatically by the randrange() function.
 
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+# Separate the list of fruits and vegetables into two lists, one for fruits and one for vegetables
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+# This creates a nested list, which is a list that contains other lists. In this case, the dirty_dozen list contains two lists, one for fruits and one for vegetables. The fruits list and vegetables list are passed as parameters to the dirty_dozen list, which then creates a new list that contains the fruits and vegetables lists.
+
+print(dirty_dozen) 
+# This will print the nested list, which contains the fruits and vegetables lists
+
+print(f"{dirty_dozen[0]}\n{dirty_dozen[1]}")
+# This uses an f-string to print the first and second items in the dirty_dozen list, which are the fruits and vegetables lists. This is a more explicit way of printing the nested list, which could be useful in certain cases, such as if you were wanting to format the output in a particular way, such as printing the fruits and vegetables lists on separate lines. It can also be useful for debugging purposes, such as if you wanted to check that the nested list was created correctly.
 
