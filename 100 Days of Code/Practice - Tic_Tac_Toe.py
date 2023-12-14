1d091f2d2820c30f484a25bf8b38092152c2a0bd
jheiser81 <jheiser81@gmail.com>
2023-12-13 21:24:37 -0800

    Refactor game logic and improve user experience

--- 100 Days of Code/Practice - Tic_Tac_Toe.py
+++ 100 Days of Code/Practice - Tic_Tac_Toe.py
@@ -15,10 +15,13 @@ player_mark = ""
 computer_mark = ""
 
 # Setting up the game board grid
-row1 = ["-", "-", "-"]
-row2 = ["-", "-", "-"]
-row3 = ["-", "-", "-"]
-game_board = [row1, row2, row3]
+# Will also handle clearing/resetting the board
+def create_board():
+    global row1, row2, row3, game_board
+    row1 = ["-", "-", "-"]
+    row2 = ["-", "-", "-"]
+    row3 = ["-", "-", "-"]
+    game_board = [row1, row2, row3]
 
 # Print the game board
 def print_board():
@@ -27,17 +30,15 @@ def print_board():
 # Determine if the user or the computer goes first
 def turn_order():
     global player_mark, computer_mark # set to global so they can be used outside the function
-    player_turn = random.choice([True, False]) # Turn order is randomly selected from a list of boolean values
-    if player_turn == True:
-        player_mark = "X"
-        computer_mark = "O"
+    player_mark = "X"
+    computer_mark = "O"
+    is_player_turn = random.choice([True, False]) # randomly selected from list of boolean values
+    
+    if is_player_turn:
         print("Player goes first!")
     else:
-        player_mark = "O"
-        computer_mark = "X"
         print("Computer goes first!")
-        computer_turn()
-    return player_mark, computer_mark
+    return is_player_turn
 
 '''Using random.randint() to randomly select from a range of integers. This requires subtracting 1 from the range to get the correct index.
     turn_result = random.randint(1, 2)
@@ -69,21 +70,28 @@ def turn_order():
 def player_turn():
     print("Choose a position on the game board to place your marker in the format 'row, column'.")
     print("For example, if you want to place your marker in the top left corner, type '1, 1' to represent the first row and first column.\n")
-    print_board()
-    position = input("Enter your move: ")
-    row = int(position[0]) - 1
-    column = int(position[-1]) - 1
     
-    while game_board[row][column] != "-":
-        print("That position is already taken. Please choose another position.")
+    while True:
         position = input("Enter your move: ")
-        row = int(position[0]) - 1
-        column = int(position[-1]) - 1
-        game_board[row][column] = player_mark
-        print_board()
+        try:
+            row = int(position[0]) - 1
+            column = int(position[-1]) - 1
+            
+            if game_board[row][column] != "-":
+                print("That position is already taken. Please choose another position.")
+            else:
+                break
+        except (IndexError, ValueError):
+            print("Invalid input. Please enter your move in the format 'row,column'.") 
+                
+    game_board[row][column] = player_mark
+    print_board()
         
 # Function for the computer's turn   
 def computer_turn():
+    print("Computer is comtemplating its move...")
+    time.sleep(1.5)
+    
     # Choose a random row and column for the computer
     row = random.randint(0, 2)
     column = random.randint(0, 2)
@@ -94,7 +102,9 @@ def computer_turn():
     # If the move is not valid, choose a new random row and column
         row = random.randint(0, 2)
         column = random.randint(0, 2)
-        
+    
+    # Display the computer's move
+    print(f"Computer places {computer_mark} on row {row+1}, column {column+1}.\n")    
     # Place computer's marker on the game board and print the board
     game_board[row][column] = computer_mark
     print_board()
@@ -102,31 +112,32 @@ def computer_turn():
 # Function to determine if the game is over.
 # Checks if any of the rows, columns, or diagonals are filled with the same marker. 
 def win_check(mark):
-    win_detected = False
+    # win_detected = False
     
     # Check horizontal rows
     if (game_board[0][0] == mark and game_board[0][1] == mark and game_board[0][2] == mark) or \
-        (game_board[1][0] == mark and game_board[1][1] == mark or game_board[1][2] == mark) or \
+        (game_board[1][0] == mark and game_board[1][1] == mark and game_board[1][2] == mark) or \
         (game_board[2][0] == mark and game_board[2][1] == mark and game_board[2][2] == mark):
-        print(f"Player ({player_mark}) wins!")
-        win_detected = True
+        return True
+        # print(f"Player ({mark}) wins!")
+        # win_detected = True
         
     # Check vertical columns 
-    if not win_detected and \
-        ((game_board[0][0] == mark and game_board[1][0] == mark and game_board[2][0] == mark) or \
+    if (game_board[0][0] == mark and game_board[1][0] == mark and game_board[2][0] == mark) or \
         (game_board[0][1] == mark and game_board[1][1] == mark and game_board[2][1] == mark) or \
-        (game_board[0][2] == mark and game_board[1][2] == mark and game_board[2][2] == mark)):
-        print(f"Player ({player_mark}) wins!")
-        win_detected = True
+        (game_board[0][2] == mark and game_board[1][2] == mark and game_board[2][2] == mark):
+        return True
+        # print(f"Player ({mark}) wins!")
+        # win_detected = True
         
     # Check diagonals
-    if not win_detected and \
-        ((game_board[0][0] == mark and game_board[1][1] == mark and game_board[2][2] == mark) or \
-        (game_board[0][2] == mark and game_board[1][1] == mark and game_board[2][0] == mark)):
-        print(f"Player ({player_mark}) wins!")
-        win_detected = True
+    if (game_board[0][0] == mark and game_board[1][1] == mark and game_board[2][2] == mark) or \
+        (game_board[0][2] == mark and game_board[1][1] == mark and game_board[2][0] == mark):
+        return True
+        # print(f"Player ({mark}) wins!")
+        # win_detected = True
         
-    return win_detected 
+    return False 
 
 # Function to determine if the game is a draw
 def draw_check():
@@ -135,32 +146,18 @@ def draw_check():
         print("The game is a draw.")
         draw_detected = True
     return draw_detected
-    
-'''alternate method using a nested for loop
-    for row in game_board:
-        for column in row:
-            if column == "-":
-                return False
-    return True'''
-   
-# Welcome message  
-print("Welcome to TicTacToe!")
-print("The game board is a 3x3 grid. Each player takes turns placing their marker on the board.")
-print("The first player to get three of their markers in a row (horizontally, vertically, or diagonally) wins.")
-print("If neither player can get three in a row, the game is a draw.\n")
 
 # Main game loop
 def game_loop():
     print("Rolling to see who goes first...\n")
     time.sleep(2) # 2 second delay
-    player_mark, computer_mark = turn_order()
+    is_player_turn = turn_order()
     print(f"Player is {player_mark} and computer is {computer_mark}.\n")
-    print("Let's begin!\n")
-    print_board()
+    
     
     while True:
         # If player goes first
-        if player_mark == "X":
+        if is_player_turn:
             player_turn()
             
             # Check if player has won  
@@ -203,22 +200,25 @@ def game_loop():
             elif draw_check():
                 print("The game is a draw.")
                 break
-            
-         
-            
-# Ask the player if they want to play again
-# If yes, reset the game board. If no, thank the player and end the game.
-input("Do you want to play again? (Y/N)").upper()
+                  
+# Welcome message and game start
+print("Welcome to TicTacToe!")
+print("The game board is a 3x3 grid. Each player takes turns placing their marker on the board.")
+print("The first player to get three of their markers in a row (horizontally, vertically, or diagonally) wins.")
+print("If neither player can get three in a row, the game is a draw.\n")
+print("Let's begin!\n")
 
+while True:
+    create_board()
+    game_loop()
+    print_board()
     
-    
+    # Ask the player if they want to play again
+    # If yes, reset the game board. If no, thank the player and end the game.
+    play_again = input("Do you want to play again? Y/N: ").upper()
+    if play_again != "Y":
+        break
 
-# What is the next logical step? How do I get the computer to make a move after the player makes a move?
-
-# How do I get the player to make a move after the computer makes a move?
-# How do I determine if the game is over?
-# How do I determine if the game is a draw?
-# How do I restart the game?
 
 
 
