79d44f33eca214c43594a487831dcca64afa372f
jheiser81 <jheiser81@gmail.com>
2023-12-15 20:16:12 -0800

    Refactor if/else statements and add rollercoaster ride check

--- 100 Days of Code/Day 3 - Conditional Statements, Logical Operators, Code Blocks, Scope.py
+++ 100 Days of Code/Day 3 - Conditional Statements, Logical Operators, Code Blocks, Scope.py
@@ -3,18 +3,18 @@
 # ======================================================== #
 
 # A basic if/else statement in python looks like this:
-# water_level = 50
-# if water_level > 80:
-#     print("Drain water")
-# else:
-#     print("Continue filling")
+water_level = 50
+if water_level > 80:
+    print("Drain water")
+else:
+    print("Continue filling")
 
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
-# if height >= 120:
-#     print("You are allowed to ride the rollercoaster.") 
-# else:
-#     print("Sorry, you are too short to ride.")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You are allowed to ride the rollercoaster.") 
+else:
+    print("Sorry, you are too short to ride.")
 # Essentially, an if/else statement is a boolean true/false statement with certain conditions. If the condition is true, a particular thing will happen. If the condition is not true (false) then something else will happen
 # In Python, if/else statements end with a colon to denote the end of the statement. The next line is indented, to show that it is part of the previous code block. This is different from other languages, which use curly braces to denote code blocks. Python instead uses indentation to denote code blocks, so it is important to use indentation properly, or the code will throw and error.
 
